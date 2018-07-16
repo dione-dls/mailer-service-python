9044bc1e7afca21032c54c5807d10496c6f6f409
Dione Estabillo <dione.estabillo@gmail.com>
2018-07-16 07:40:14 +0000

    Setup; Add unit test

---
+++ tests/test_mail_service.py
@@ -0,0 +1,35 @@
+from nameko.testing.services import worker_factory
+
+
+import sys
+sys.path.insert(1, '..')
+
+from MailService import MailService
+from PaymentService import PaymentService
+
+class TestMailService(object):
+    def get_payload(self):
+        return {
+            'client': {
+                'name': 'John Doe',
+                'email': 'john.doe@mailexample.com'
+            },
+            'payee': {
+                'name': 'Jane Doe',
+                'email': 'jane.doe@mailexample.com'
+            },
+            'payment': {
+                'amount': 9000,
+                'currency': 'USD'
+            }
+        }
+
+    def test_mail_service_unit(self):
+        mail_service = worker_factory(MailService)
+        payload = self.get_payload()
+        
+        assert mail_service.on_payment_received(payload)
+        print(mail_service.on_payment_received(payload))
+
+        del payload['payee']
+        assert mail_service.on_payment_received(payload) == False
